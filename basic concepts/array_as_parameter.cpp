009490081a64e6a1ffe58b28bdbb74bc1822e7bd
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-03-29 20:29:24 +0530

    update

--- basic concepts/array_as_parameter.cpp
+++ basic concepts/array_as_parameter.cpp
@@ -59,3 +59,68 @@ int main()
     A=funct(5);
 return 0;
 };
+
+
+
+
+#include <iostream>
+using namespace std;
+/*
+void fun(int a[],int n)//passing pointer to an array,size of array
+{
+    int i;
+    a[0]=6;//modifying value of array
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i];
+    }
+};
+*/
+int * funct(int n)
+{
+    int *p;
+    p=(int *) malloc(n*sizeof(int));
+    return (p);
+
+}
+int main()
+{
+    //int a[5]={1,2,3,4,5};
+    //fun(a,5);
+    int *A;
+    A=funct(5);
+return 0;
+};
+
+
+
+
+
+#include <iostream>
+using namespace std;
+/*
+void fun(int a[],int n)//passing pointer to an array,size of array
+{
+    int i;
+    a[0]=6;//modifying value of array
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i];
+    }
+};
+*/
+int * funct(int n)
+{
+    int *p;
+    p=(int *) malloc(n*sizeof(int));
+    return (p);
+
+}
+int main()
+{
+    //int a[5]={1,2,3,4,5};
+    //fun(a,5);
+    int *A;
+    A=funct(5);
+return 0;
+};
