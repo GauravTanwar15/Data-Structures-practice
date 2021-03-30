c90514c27f97dfd6e889a0fe81d973a2523d461f
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-03-30 20:05:32 +0530

    update

--- recursion concepts/factorial-using-recursion.cpp
+++ recursion concepts/factorial-using-recursion.cpp
@@ -14,3 +14,36 @@ int main(){
 
 return 0;
 }
+
+#include <iostream>
+using namespace std;
+int factorial(int n)
+{
+    if(n==0)
+        return 1;
+    else
+        return factorial(n-1)*n;
+}
+int main(){
+    int n;
+   n= factorial(6);
+   cout<<n;//720
+
+return 0;
+}
+#include <iostream>
+using namespace std;
+int factorial(int n)
+{
+    if(n==0)
+        return 1;
+    else
+        return factorial(n-1)*n;
+}
+int main(){
+    int n;
+   n= factorial(6);
+   cout<<n;//720
+
+return 0;
+}
