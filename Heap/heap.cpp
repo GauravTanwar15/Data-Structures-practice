b32ad8df6977960286f43d7d26cd5eef1330b2b6
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-04-21 18:53:28 +0530

    Add Heap Solution

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
+
+void Insert(int A[], int n)
+{
+    int i=n, temp;
+    temp =A[i];
+
+    while (i>1 && temp > A[i/2])
+    {
+        A[i]=A[i/2];
+        i=i/2;
+    }
+
+    A[i]=temp;
+}
+
 int main(){
-int H[] = {0,10,20,30,40,50,60}; 
+    int H[] = {0,2,5,8,9,10,7}; 
+    Insert(H,2);
+    Insert(H,5);
+    Insert(H,8);
+    Insert(H,9);
+
 return 0;
 };
