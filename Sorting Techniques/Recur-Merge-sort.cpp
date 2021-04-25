303c7a6ab8ccd248f500415c30946dbd1e0bce5a
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-04-25 13:44:37 +0530

    Add Merge Sort

--- Sorting Techniques/Recur-Merge-sort.cpp
+++ Sorting Techniques/Recur-Merge-sort.cpp
@@ -1,4 +1,3 @@
-Recursive Merge Sort 
 #include <stdio.h>
 #include<stdlib.h>
 void swap(int *x,int *y)
@@ -42,10 +41,11 @@ int main()
 {
  int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
  
- MergeSort(A,n);
+ MergeSort(A,0,9);
  
  for(i=0;i<10;i++)
  printf("%d ",A[i]);
  printf("\n");
  
  return 0;
+};
